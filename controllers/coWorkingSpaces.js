eae303eb951bae648ab25ea5e12604a2a012c9c2
DecQmenT ⠀ <decqment@Nong-Earn.local>
2024-02-29 23:04:13 +0700

    a5 done

--- controllers/coWorkingSpaces.js
+++ controllers/coWorkingSpaces.js
@@ -1,22 +1,60 @@
+const CoWorkingSpace = require('../models/CoWorkingSpace');
 
-exports.getCoWorkingSpaces = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Show all co-working spaces" });
-  };
-  exports.getCoWorkingSpace = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Show co-working space ${req.params.id}` });
-  };
-  exports.createCoWorkingSpace = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Create new co-working space" });
-  };
-  exports.updateCoWorkingSpace = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Update co-working space ${req.params.id}` });
-  };
-  exports.deleteCoWorkingSpace = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Delete co-working space ${req.params.id}` });
-  };
+exports.getCoWorkingSpaces = async (req, res, next) => {
+  try {
+    const coWorkingSpaces = await CoWorkingSpace.find();
+    res.status(200).json({success: true, count: coWorkingSpaces.length, data: coWorkingSpaces});
+  } catch(err) {
+    res.status(400).json({success: false});
+  }
+};
+  
+exports.getCoWorkingSpace = async (req, res, next) => {
+  try {
+    const coWorkingSpace = await CoWorkingSpace.findById(req.params.id);
+
+    if(!coWorkingSpace) {
+      return res.status(400).json({success: false});
+    }
+
+    res.status(200).json({success: true, data: coWorkingSpace});
+  } catch(err) {
+    res.status(400).json({success: false});
+  }
+};
+  
+exports.createCoWorkingSpace = async (req, res, next) => {
+  const coWorkingSpace = await CoWorkingSpace.create(req.body);
+  res.status(201).json({success: true, data: coWorkingSpace});
+};
+  
+exports.updateCoWorkingSpace = async (req, res, next) => {
+  try {
+    const coWorkingSpace = await CoWorkingSpace.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if(!coWorkingSpace) {
+      return res.status(400).json({success: false});
+    }
+
+    res.status(200).json({success: true, data: coWorkingSpace});
+  } catch(err) {
+    res.status(400).json({success: false});
+  }
+};
+  
+exports.deleteCoWorkingSpace = async (req, res, next) => {
+  try {
+    const coWorkingSpace = await CoWorkingSpace.findByIdAndDelete(req.params.id);
+
+    if(!coWorkingSpace) {
+      return res.status(400).json({success: false});
+    }
+
+    res.status(200).json({success: true, data: {}});
+  } catch(err) {
+    res.status(400).json({success: false});
+  }
+};
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const coWorkingSpace = require('./routes/coWorkingSpaces');
 
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
 const app = express();
 
+app.use(express.json());
 app.use('/api/v1/co-working-space', coWorkingSpace);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port',PORT));
 
-process.on('unhandled')
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
