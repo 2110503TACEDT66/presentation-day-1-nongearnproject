79ba3470160529ae6436709b7f018990f9c4cb7a
DecQmenT ⠀ <decqment@Nong-Earn.local>
2024-03-03 20:10:45 +0700

    new feature

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,172 +1,208 @@
-const Booking = require('../models/Booking');
-const CoWorkingSpace = require('../models/CoWorkingSpace');
+const Booking = require("../models/Booking");
+const CoWorkingSpace = require("../models/CoWorkingSpace");
+const User = require("../models/User");
 
 exports.getBookings = async (req, res, next) => {
-    let query;
+  let query;
 
-    if(req.user.role !== 'admin') {
-        query = Booking.find({user: req.user.id}).populate({
-            path: 'coworkingspace',
-            select: 'name tel address'
-        });
+  if (req.user.role !== "admin") {
+    query = Booking.find({ user: req.user.id }).populate({
+      path: "coworkingspace",
+      select: "name tel address",
+    });
+  } else {
+    if (req.params.coworkingspaceId) {
+      console.log(req.params.coworkingspaceId);
+      query = Booking.find({
+        coworkingspace: req.params.coworkingspaceId,
+      }).populate({
+        path: "coworkingspace",
+        select: "name tel address",
+      });
     } else {
-        if (req.params.coworkingspaceId) {
-            console.log(req.params.coworkingspaceId);
-            query = Booking.find({coworkingspace: req.params.coworkingspaceId}).populate({
-                path: 'coworkingspace',
-                select: 'name tel address'
-            });
-        } else {
-            query = Booking.find().populate({
-                path: 'coworkingspace',
-                select: 'name tel address'
-            });
-        }
+      query = Booking.find().populate({
+        path: "coworkingspace",
+        select: "name tel address",
+      });
     }
+  }
 
-    try {
-        const bookings = await query;
+  try {
+    const bookings = await query;
 
-        res.status(200).json({
-            success: true,
-            count: bookings.length,
-            data: bookings
-        });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot find Booking"
-        });
-    }
-}
+    res.status(200).json({
+      success: true,
+      count: bookings.length,
+      data: bookings,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot find Booking",
+    });
+  }
+};
 
 exports.getBooking = async (req, res, next) => {
-    try {
-        const booking = await Booking.findById(req.params.id).populate({
-            path: 'coworkingspace',
-            select: 'name description tel'
-        });
+  try {
+    const booking = await Booking.findById(req.params.id).populate({
+      path: "coworkingspace",
+      select: "name description tel",
+    });
 
-        if(!booking) {
-            return res.status(404).json({
-                success: false, 
-                message: `No booking with the id of ${req.params.id}`
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            data: booking
-        });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot find Booking"
-        });
+    if (!booking) {
+      return res.status(404).json({
+        success: false,
+        message: `No booking with the id of ${req.params.id}`,
+      });
     }
-}
+
+    res.status(200).json({
+      success: true,
+      data: booking,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot find Booking",
+    });
+  }
+};
 
 exports.addBooking = async (req, res, next) => {
-    try {
+  try {
+    req.body.coworkingspace = req.params.coworkingspaceId;
+    console.log(req.params.coworkingspaceId);
+    const coworkingspace = await CoWorkingSpace.findById(
+      req.params.coworkingspaceId
+    );
 
-        req.body.coworkingspace = req.params.coworkingspaceId;
-        console.log(req.params.coworkingspaceId)
-        const coworkingspace = await CoWorkingSpace.findById(req.params.coworkingspaceId);
-
-
-        if (!coworkingspace) {
-            return res.status(404).json({
-                success: false,
-                message: `No coworkingspace with the id of ${req.params.coworkingspaceId}`
-            });
-        }
-
-        req.body.user = req.user.id;
-        
-        const existedBookings = await Booking.find({user: req.user.id});
-        if(existedBookings.length >= 3 && req.user.role != 'admin') {
-            return res.status(400).json({
-                success: false,
-                message: `The user with ID ${req.user.id} has already made 3 bookings`
-            });
-        }
-        console.log("asd");
-        const booking = await Booking.create(req.body);
-        
-        res.status(200).json({
-            success: true,
-            data: booking
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot create Booking"
-        });
+    if (!coworkingspace) {
+      return res.status(404).json({
+        success: false,
+        message: `No coworkingspace with the id of ${req.params.coworkingspaceId}`,
+      });
     }
-}
 
+    req.body.user = req.user.id;
+
+    const existedBookings = await Booking.find({ user: req.user.id });
+    if (existedBookings.length >= 3 && req.user.role != "admin") {
+      return res.status(400).json({
+        success: false,
+        message: `The user with ID ${req.user.id} has already made 3 bookings`,
+      });
+    }
+    const booking = await Booking.create(req.body);
+
+    // new feature
+    const user = await User.findById(req.user.id);
+    user.countBooking += 1;
+    await user.save();
+    console.log(user);
+
+    res.status(200).json({
+      success: true,
+      data: booking,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot create Booking",
+    });
+  }
+};
 
 exports.updateBooking = async (req, res, next) => {
-    try {
-        let booking = await Booking.findById(req.params.id);
+  try {
+    let booking = await Booking.findById(req.params.id);
 
-        if(!booking) {
-            return res.status(404).json({
-                success: false, 
-                message: `No booking with the id of ${req.params.id}`
-            });
-        }
-
-        if(booking.user.toString() !== req.user.id && req.user.role != 'admin') {
-            return res.status(401).json({
-                success: false,
-                message: `User ${req.user.id} is not authorized to delete this bootcamp`
-            });
-        }
-
-        booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
-
-        res.status(200).json({
-            success: true,
-            data: booking
-        });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot update Booking"
-        });
+    if (!booking) {
+      return res.status(404).json({
+        success: false,
+        message: `No booking with the id of ${req.params.id}`,
+      });
     }
-}
+
+    if (booking.user.toString() !== req.user.id && req.user.role != "admin") {
+      return res.status(401).json({
+        success: false,
+        message: `User ${req.user.id} is not authorized to delete this bootcamp`,
+      });
+    }
+
+    booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: booking,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot update Booking",
+    });
+  }
+};
 
 exports.deleteBooking = async (req, res, next) => {
-    try {
-        const booking = await Booking.findById(req.params.id);
+  try {
+    const booking = await Booking.findById(req.params.id);
 
-        if(!booking) {
-            return res.status(404).json({
-                success: false, 
-                message: `No booking with the id of ${req.params.id}`
-            });
-        }
-
-        await Booking.deleteOne();
-        
-        res.status(200).json({
-            success: true,
-            data: {}
-        });
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Cannot delete Booking"
-        });
+    if (!booking) {
+      return res.status(404).json({
+        success: false,
+        message: `No booking with the id of ${req.params.id}`,
+      });
     }
-}
+
+    await Booking.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot delete Booking",
+    });
+  }
+};
+
+exports.getTop1BookingUser = async (req, res, next) => {
+  try {
+    let usersWithMaxCountBooking;
+    const maxCountBooking = await User.findOne()
+      .sort({ countBooking: -1 })
+      .exec();
+
+    if (maxCountBooking) {
+        usersWithMaxCountBooking = await User.find({
+        countBooking: maxCountBooking.countBooking,
+      });
+
+      console.log(usersWithMaxCountBooking);
+    } else {
+      console.log("No users found.");
+    }
+    res.status(200).json({
+        success : "true",
+        data : usersWithMaxCountBooking,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot find best user booking",
+    });
+  }
+};
--- routes/bookings.js
+++ routes/bookings.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const {getBookings, getBooking, addBooking, updateBooking, deleteBooking} = require('../controllers/bookings');
+const {getBookings, getBooking, addBooking, updateBooking, deleteBooking, getTop1BookingUser} = require('../controllers/bookings');
 
 const router = express.Router({mergeParams: true});
 
 const {protect, authorize} = require('../middleware/auth');
 
+router.route('/bestUser')
+    .get(protect, authorize('admin'), getTop1BookingUser);
 router.route('/')
     .get(protect, getBookings)
     .post(protect, authorize('admin', 'user'), addBooking);    
