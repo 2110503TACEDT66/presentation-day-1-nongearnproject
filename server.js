7bd465ec87f67c77e41fa497e7bb630b0d0c67ac
focustoplaner <focustoplaner@hotmail.com>
2024-03-25 19:33:18 +0700

    vercel added

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 const {xss} = require('express-xss-sanitizer');
 
 dotenv.config({path: './config/config.env'});
@@ -45,7 +48,26 @@ app.use('/api/v1/bookings', bookings);
 app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, "on" + process.env.HOST + " :", PORT));
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A CoWorkingSpace Booking API'
+        },
+        servers: [
+            {
+                url: process.env.HOST + ':' + PORT + '/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
